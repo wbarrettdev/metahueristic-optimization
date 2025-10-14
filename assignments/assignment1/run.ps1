e11c9ef97339c2a0a9f0455c31db16964ac54af9
William Barrett <wbarrettdev@gmail.com>
2025-10-14 09:30:24 +0100

    udpate

--- assignments/assignment1/run.ps1
+++ assignments/assignment1/run.ps1
@@ -1,3 +1,4 @@
 echo "#RUN 1##################################################################################"
-python Barrett_William_R00029480_MH1.py data/inst-b.tsp 10 1000 100 0 0.9 0 0.2 0.1 0.5
-
+#python Barrett_William_R00029480_MH1.py data/inst-b.tsp 10 1000 100 1 0.9 1 0.2 0.1 0.5
+python Barrett_William_R00029480_MH1.py data/inst-
+b.tsp 10 5000 300 0 0.9 1 0.5 0 0.5
