f86fdb25a52d1fc93cfa65389c6bbb6fa4017d10
William Barrett <wbarrettdev@gmail.com>
2025-11-25 21:59:33 +0000

    update before hsat tabu

---
+++ assignments/assignment2/run.ps1
@@ -0,0 +1,12 @@
+# Default Settings
+python Barrett_R00029480_MH2.py data/ gsat 10 50 500 0.1 10
+
+# pyhton file inst_dir alg nRuns nRestarts nFlips wp tl
+# alg: one of { gwsat, hsat, walksat, hsatTabu, grimesHsat, grimesWsat}
+# nRuns: the number of runs of the algorithm to perform
+# nRestarts: is the number of restarts
+# nFlips: is the number of successive iterations without an improving move before restarting
+# wp: is the random walk probability
+# tl: is the tabu length
+
+
